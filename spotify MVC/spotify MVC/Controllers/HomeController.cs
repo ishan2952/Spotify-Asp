deabe5fd55b568737bfba90e72d9eb87be69d736
Ishan <ishuganatra2952@gmail.com>
2022-08-19 00:30:10 -0300

    aa

--- spotify MVC/spotify MVC/Controllers/HomeController.cs
+++ spotify MVC/spotify MVC/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using spotify_MVC.Models;
+using SpotifyAPI.Web;
 using System.Diagnostics;
 
 namespace spotify_MVC.Controllers
@@ -13,8 +14,32 @@ namespace spotify_MVC.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var config = SpotifyClientConfig
+                .CreateDefault()
+                .WithAuthenticator(new ClientCredentialsAuthenticator("44dc3df445894b2e95eda36bd59c892d",
+                "2ac5fbc05fda4368b994fdb616087cb1"));
+            var spotify = new SpotifyClient(config);
+
+            FeaturedPlaylistsResponse track = await spotify.Browse.GetFeaturedPlaylists();
+            var track2 = await spotify.Browse.GetNewReleases();
+            //Console.WriteLine(track.Artists[0].Name + " - " + track.Name);
+            ViewBag.track = track;
+            return View();
+        }
+
+        public async Task<IActionResult> Index2()
+        {
+            var config = SpotifyClientConfig
+                .CreateDefault()
+                .WithAuthenticator(new ClientCredentialsAuthenticator("44dc3df445894b2e95eda36bd59c892d",
+                "2ac5fbc05fda4368b994fdb616087cb1"));
+            var spotify = new SpotifyClient(config);
+
+            NewReleasesResponse track2 = await spotify.Browse.GetNewReleases();
+            //Console.WriteLine(track.Artists[0].Name + " - " + track.Name);
+            ViewBag.track = track2;
             return View();
         }
 
